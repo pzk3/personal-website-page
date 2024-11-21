2d2b9662f8d9674b9f89e5cbbbba4f2c8e914f73
pzk3 <109077545+pzk3@users.noreply.github.com>
2024-11-21 22:34:06 +1000

    update 1

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Mihir Nepal",
+  DESCRIPTION: "Welcome my website.",
+  AUTHOR: "Mihir Nepal",
 }
 
 // Work Page
@@ -56,26 +56,25 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "example@example.com",
+    HREF: "mailto:example@example.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "example",
+    HREF: "https://github.com/example"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "example",
+    HREF: "https://www.linkedin.com/in/example/",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "example",
+    HREF: "https://twitter.com/example",
   },
 ]
-
